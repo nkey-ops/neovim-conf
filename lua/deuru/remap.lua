586cc2388fbb8fad089f1ab637d382811711417b
deuru <terrythebear746@gmail.com>
2024-02-13 13:13:08 +0400

    Added: keymaps to go through buffers. Updated: minor changes to set config file.

--- lua/deuru/remap.lua
+++ lua/deuru/remap.lua
@@ -26,9 +26,6 @@ vim.keymap.set("n", "<leader>Y", [["+Y]])
 
 vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 
--- This is going to get me cancelled
-vim.keymap.set("i", "<C-c>", "<Esc>")
-
 vim.keymap.set("n", "Q", "<nop>")
 vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
@@ -41,19 +38,23 @@ vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
-vim.keymap.set("n", "<leader>vpp", "<cmd>e ~/.dotfiles/nvim/.config/nvim/lua/theprimeagen/packer.lua<CR>");
 vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>");
 
 vim.keymap.set("n", "<leader><leader>", function()
   vim.cmd("so")
 end)
 
+vim.keymap.set("n", "[b", "<cmd>bprevious<CR>")
+vim.keymap.set("n", "]b", "<cmd>bnext<CR>")
+vim.keymap.set("n", "[B", "<cmd>bfirst<CR>")
+vim.keymap.set("n", "]B", "<cmd>blast<CR>")
+
 
 -- enable syntax check
 vim.keymap.set("n", "<leader>esp", "<cmd>set spell spelllang=en_us<CR>");
 vim.keymap.set("n", "<leader>dsp", "<cmd>set nospell<CR>");
 
--- exit term usign Esc
+-- exit term using Esc
 vim.keymap.set('t', '<Esc>', "<C-\\><C-n>")
 -- open terminal window at current dir
 vim.keymap.set("n", "<leader>tc",
@@ -72,20 +73,7 @@ vim.api.nvim_create_user_command("Cppath", function()
 end, {})
 
 
--- : Substitute
---   [w]ord under cursor
---   [/] last search pattern
---   ["] from default register
---   [.] last inserted text
+vim.cmd("cnoremap <expr> %% getcmdtype() == ':' ? expand('%:h').'/' : '%%'")
 
---vim.keymap.set("n", "<leader>gsw",  ">%s/<C-r><C-w>//g<left><left><CR>")
---nnoremap gs/ :%s/<c-r>///g<left><left>
---nnoremap gs" :%s/<c-r>"//g<left><left>
---nnoremap gs. :%s/<c-r>.//g<left><left>
---xnoremap gsw y:<c-u>%s/<c-r>"//g<left><left>
---xnoremap gs/ :s/<c-r>///g<left><left>
---xnoremap gs" :s/<c-r>"//g<left><left>
---xnoremap gs. :s/<c-r>.//g<left><left>
---" Change word under cursor
---nnoremap c* *``cgn
---nnoremap c# #``cgN
+
+
--- lua/deuru/set.lua
+++ lua/deuru/set.lua
@@ -17,23 +17,22 @@ vim.opt.backup = false
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
 
-vim.opt.hlsearch = false
+vim.opt.hlsearch = true
 vim.opt.incsearch = true
 
 vim.opt.termguicolors = true
 
-vim.opt.scrolloff = 8
+vim.opt.scrolloff = 4
 vim.opt.signcolumn = "yes"
 vim.opt.isfname:append("@-@")
 
 vim.opt.updatetime = 50
-
 vim.opt.colorcolumn = "100"
-
 vim.opt.clipboard='unnamedplus'
-
-vim.opt.hlsearch = true
-
-vim.opt.showtabline = 0
+vim.opt.showtabline = 1
 
 vim.opt.completeopt = {'menu', 'menuone', 'noselect'}
+vim.foldenable = false
+
+vim.cmd("packadd! matchit")
+vim.cmd("packadd matchit")

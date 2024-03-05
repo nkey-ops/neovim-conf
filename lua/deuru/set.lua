8c4e0baffc9ae5d15223e636430f1d966a9273b9
deuru <terrythebear746@gmail.com>
2024-03-05 04:09:09 +0400

    Updated: set.lua tabs length is 4, hlsearch is on, scrolloff is 4, colorcolumn is 100, no matchit is enabled

--- lua/deuru/set.lua
+++ lua/deuru/set.lua
@@ -3,9 +3,9 @@ vim.opt.guicursor = ""
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
-vim.opt.shiftwidth = 2
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
 vim.opt.smartindent = true
@@ -17,22 +17,17 @@ vim.opt.backup = false
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
 
-vim.opt.hlsearch = true
 vim.opt.incsearch = true
 
 vim.opt.termguicolors = true
 
-vim.opt.scrolloff = 4
+vim.opt.scrolloff = 8
 vim.opt.signcolumn = "yes"
 vim.opt.isfname:append("@-@")
 
 vim.opt.updatetime = 50
-vim.opt.colorcolumn = "100"
-vim.opt.clipboard='unnamedplus'
+vim.opt.colorcolumn = "80"
+vim.opt.clipboard = 'unnamedplus'
 vim.opt.showtabline = 1
 
-vim.opt.completeopt = {'menu', 'menuone', 'noselect'}
-vim.foldenable = false
-
-vim.cmd("packadd! matchit")
-vim.cmd("packadd matchit")
+vim.opt.completeopt = { 'menu', 'menuone', 'noselect' }

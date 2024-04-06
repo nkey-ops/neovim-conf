e56119cc258bcafdb1b0714facec87434ad9b239
deuru <terrythebear746@gmail.com>
2024-04-06 22:33:32 +0400

    Fixed: closing float window with <Esc> key Added: markdown linebrek, cursor line

--- after/plugin/configs.lua
+++ after/plugin/configs.lua
@@ -1,11 +1,18 @@
- vim.keymap.set({'n'}, '<C-[>', function()
-        local buffer = vim.fn.win_getid()
+vim.keymap.set({ 'n' }, '<Esc>', function()
+    local buffer = vim.fn.win_getid()
 
-         if  vim.api.nvim_win_is_valid(buffer) and
-             vim.api.nvim_win_get_config(buffer).relative ~= '' then
+    if vim.api.nvim_win_is_valid(buffer) and
+        vim.api.nvim_win_get_config(buffer).relative ~= '' then
+        vim.api.nvim_win_close(buffer, false)
+    end
+end, { desc = "Close floating window" })
 
-             vim.api.nvim_win_close(buffer, false)
-         end
- end, {desc = "Close floating window"})
---
---
+vim.api.nvim_create_autocmd({ "FileType" }, {
+    pattern = 'markdown',
+    callback = function()
+        vim.cmd("setlocal linebreak")
+        vim.cmd("setlocal breakindent")
+        vim.cmd("setlocal breakindentopt=shift:2,list:2")
+        vim.cmd("setlocal wrap")
+    end
+})

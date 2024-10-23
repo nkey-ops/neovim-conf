283a2aa3f8020d75f48a5938edfd1799c627e54f
nkey-ops <terrythebear746@gmail.com>
2024-10-23 20:16:42 +0400

    removed: the keybind for closing a tab

--- lua/deuru/conf/tabby-init.lua
+++ lua/deuru/conf/tabby-init.lua
@@ -110,7 +110,6 @@ require('tabby').setup({
 })
 
 vim.api.nvim_set_keymap("n", "<leader>ta", ":$tabnew<CR>", { noremap = true })
-vim.api.nvim_set_keymap("n", "<leader>tc", ":tabclose<CR>", { noremap = true })
 vim.api.nvim_set_keymap("n", "<leader>to", ":tabonly<CR>", { noremap = true })
 vim.api.nvim_set_keymap("n", "<leader>tn", ":tabn<CR>", { noremap = true })
 vim.api.nvim_set_keymap("n", "<leader>tp", ":tabp<CR>", { noremap = true })

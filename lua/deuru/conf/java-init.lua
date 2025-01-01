16db28121d00cbc806f561a65c3b9058943e02f5
nkey-ops <terrythebear746@gmail.com>
2025-01-01 23:10:47 +0400

    added: 5pos formatting, add method code action

--- lua/deuru/conf/java-init.lua
+++ lua/deuru/conf/java-init.lua
@@ -1,5 +1,7 @@
 local local_marks = require("extended-marks.local")
 
+local M = {}
+
 local jdtls = require("jdtls")
 local mason_registry = require('mason-registry')
 local google_java_format_jar =
@@ -10,37 +12,60 @@ local google_java_format_jar =
     )
 
 local enable_auto_format = false
-local java_formats = { "google2", "google4", "intellij" }
+local java_formats = { "google2", "google4", "intellij", "5pos" }
 local java_format = java_formats[2]
 
 local set = vim.keymap.set
+
+local java_path = "/usr/lib/jvm/java-21-openjdk-amd64/bin/java"
 
 require('formatter').setup {
     filetype = {
         java = {
             function()
-                if java_format:match("intellij") then
+                local lines = nil
+
+                local mode = vim.api.nvim_get_mode()['mode']
+                P(mode)
+
+                if mode == 'v' or mode == 'V' or mode == '\22' then
+                    local startLine = vim.fn.getpos("v")[2]
+                    local endLine = vim.fn.getcurpos()[2]
+                    lines = string.format("--lines %s:%s", startLine, endLine)
+                    M.exit_visual()
+                end
+                P(lines)
+
+
+                if java_format:match("google2") then
                     return {
-                        exe = "idea format",
-                        args = { "-allowDefaults" },
-                        stdin = false,
+                        exe = java_path,
+                        args = { '-jar', google_java_format_jar,
+                            lines and lines or "",
+                            vim.api.nvim_buf_get_name(0)
+                        },
+                        stdin = true
                     }
-                elseif java_format:match("google2") then
+                elseif java_format:match("5pos") then
                     return {
-                        exe = 'java',
-                        args = { '-jar', google_java_format_jar, vim.api.nvim_buf_get_name(0) },
+                        exe = java_path,
+                        args = {
+                            '-jar', google_java_format_jar,
+                            "--aosp",
+                            "--skip-javadoc-formatting",
+                            "--skip-reflowing-long-strings",
+                            lines and lines or "",
+                            vim.api.nvim_buf_get_name(0)
+                        },
                         stdin = true
                     }
                 else
                     return {
-                        exe = "google-java-format",
-                        args = {
-                            "--aosp",
-                            vim.api.nvim_buf_get_name(0),
-
-                            "--replace",
-                        },
-                        stdin = true,
+                        exe = java_path,
+                        args = { '-jar', google_java_format_jar, "--aosp",
+                            lines and lines or "",
+                            vim.api.nvim_buf_get_name(0) },
+                        stdin = true
                     }
                 end
             end
@@ -76,7 +101,6 @@ vim.api.nvim_create_user_command("SetJavaFormat",
         complete = function() return java_formats end,
     })
 
-local M = {}
 
 local auto_format = function()
     assert(type(enable_auto_format) == 'boolean', "Should be boolean:", enable_auto_format)
@@ -90,7 +114,9 @@ end
 
 local format = function()
     local_marks.update()
+
     vim.cmd("FormatWrite")
+
     local_marks.restore()
 end
 
@@ -121,6 +147,10 @@ end
 
 local create_field = function()
     M.perform_action("Create field .*")
+end
+
+local create_method = function()
+    M.perform_action("Create method .*")
 end
 
 local add_doc = function()
@@ -169,7 +199,7 @@ local attach_java_configs = function()
             local opts = { silent = true, buffer = args.buf }
 
             set('n', '<leader>o', jdtls.organize_imports, ext(opts, { desc = "Java Import" }))
-            set('n', '<leader>f', format, ext(opts, { desc = "Java Format" }))
+            set({ 'n', 'v' }, '<leader>f', format, ext(opts, { desc = "Java Format" }))
             set({ 'n', 'v' }, '<leader>ev', extract_var, ext(opts, { desc = "Java [E]xtract [V]ariable" }))
             set({ 'n', 'v' }, '<leader>em', extract_meth, ext(opts, { desc = "Java [E]xtract [M]ethod" }))
             set("n", "<leader>jc", "<cmd>JdtCompile<CR>", ext(opts, { desc = "Java JdtCompile" }))
@@ -189,6 +219,7 @@ local attach_java_configs = function()
 
             set("n", "<leader>ccg", create_getter, opts)
             set("n", "<leader>ccf", create_field, opts)
+            set("n", "<leader>ccm", create_method, opts)
             set("n", "<leader>cd", add_doc, opts)
             set("n", "<leader>cpn", assign_param_to_new_field,
                 ext(opts, { desc = "Java: Assgin constructor param to a field" }))
@@ -196,7 +227,8 @@ local attach_java_configs = function()
             set("n", "<leader>cts", to_string, opts)
             set("n", "<leader>csc", surround_try_catch, opts)
             set("n", "<leader>cat", add_throws, opts)
-            set("n", "<leader>cim", add_unimplemented_methods, opts)
+            set("n", "<leader>cum", add_unimplemented_methods,
+                ext(opts, { desc = "Java Add [U]nimplemented [M]ethods" }))
 
             vim.api.nvim_create_autocmd("BufWritePost", {
                 pattern = "*.java",
@@ -235,7 +267,7 @@ vim.api.nvim_create_autocmd("FileType", {
 
 function M.exit_visual()
     local mode = vim.api.nvim_get_mode()['mode']
-    if mode ~= 'v' and mode ~= 'V' then
+    if mode ~= 'v' and mode ~= 'V' and mode ~= '\22' then
         error("Exit_visual(): Can't exit visual mode because it isn't in visual mode")
         return
     end

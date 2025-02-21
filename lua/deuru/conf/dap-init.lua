8bce1a4a123dd3511467d6e43ca6b6a1bea2caa1
nkey-ops <terrythebear746@gmail.com>
2025-02-22 00:49:11 +0400

    updated: dap-init : prettied configurations

--- lua/deuru/conf/dap-init.lua
+++ lua/deuru/conf/dap-init.lua
@@ -1,78 +1,105 @@
 local dap, dapui = require("dap"), require("dapui")
 local widgets = require("dap.ui.widgets")
-
--- dap.configurations.java = {
---     -- --vscode java clien args
---     -- {
---     --     type = 'java',
---     --     -- attach to a running Java program that was started with --debug-jvm
---     --     -- and that is waiting on 5005 for a debug adapter to attach
---     --     request = 'attach',
---     --     name = "Java attach",
---     --     hostName = "127.0.0.1",
---     --     port = 5005,
---     --     projectName = "Current Project",
---     -- }
--- }
-
-dap.defaults.fallback.switchbuf = "usetab, useopen, uselast"
-
-local my_sidebar = widgets.sidebar(widgets.frames)
-vim.keymap.set("n", "<leader>dws", function() my_sidebar.open() end)
 local map = vim.keymap;
 
-map.set('n', '<leader>dc', function() dap.continue() end, { desc = "Debug: [C]ontinue" })
-map.set('n', '<leader>drl', function() dap.run_last() end, { desc = "Debug: [R]un [L]ast" })
-map.set('n', '<leader>dre', function() dap.restart() end, { desc = "Debug: [R]estart" })
-map.set('n', '<leader>drc', function() dap.run_to_cursor() end, { desc = "Debug: [R]un to the [C]ursor" })
-map.set('n', '<leader>dte', function() dap.terminate() end, { desc = "Debug: [Te]minate" })
-map.set('n', '<leader>dp', function() dap.pause() end, { desc = "Debug: [P]ause" })
--- Steps
-map.set('n', '<leader>dsv', function() dap.step_over() end, { desc = "Debug: [S]tep [O]ver" })
-map.set('n', '<leader>dsi', function() dap.step_into({ askForTargets = true }) end, { desc = "Debug: [S]tep [I]nto" })
-map.set('n', '<leader>dst', function() dap.step_out() end, { desc = "Debug: [S]tep Out" })
-map.set('n', '<leader>dsb', function() dap.step_back() end, { desc = "Debug: [S]tep [B]ack" })
--- Breakpoints
-map.set('n', '<leader>dbt', function() dap.toggle_breakpoint() end, { desc = "Debug: Toggle Breakpoint" })
-map.set('n', '<leader>db', function() dap.set_breakpoint() end, { desc = "Debug: Set Breakpoint" })
-map.set('n', '<leader>dbc', function()
-    dap.set_breakpoint(
+-- dap.defaults.fallback.switchbuf = "usevisible, usetab, useopen, uselast"
+dap.defaults.fallback.switchbuf = "uselast"
+
+local function step_into()
+    return dap.step_into({ askForTargets = true })
+end
+local function conditional_breakpoint()
+    return dap.set_breakpoint(
         vim.fn.input("Condition: "),
         vim.fn.input("Times Hit: "),
         vim.fn.input("Log message: "))
-end, { desc = "Debug: Set Breakpoint with Conditions" })
-map.set('n', '<leader>dbl', function() dap.set_breakpoint(nil, nil, vim.fn.input('Log message: ')) end,
-    { desc = "Debug: Set [B]reakpoint with [l]og message" })
--- map.set('n', '<leader>dbe', function() dap.set_exception_breakpoints() end, { desc = "Debug: [C]lear [B]reakpoints" })
-map.set('n', '<leader>dbl', function() dap.list_breakpoints() end, { desc = "Debug: [L]ist [B]reakpoints" })
-map.set('n', '<leader>dbd', function() dap.clear_breakpoints() end, { desc = "Debug: [C]lear [B]reakpoints" })
+end
+local function log_breakpoint()
+    dap.set_breakpoint(nil, nil, vim.fn.input('Log message: '))
+end
+local function float_frames() widgets.centered_float(widgets.frames) end
+local function float_scopes() widgets.centered_float(widgets.scopes) end
 
--- map.set('n', '<C-d>p', function() dap.up() end, { desc = "Debug: [S]tacktrace Up" })
--- map.set('n', '<C-d>n', function() dap.down() end, { desc = "Debug: [S]tacktrace Down" })
+-- Run
+map.set('n', '<leader>drl', dap.run_last, --[[----------]] { desc = "Debug: [R]un [L]ast" })
+map.set('n', '<leader>drr', dap.restart, --[[-----------]] { desc = "Debug: [R]un [R]estart" })
+map.set('n', '<leader>drt', dap.run_to_cursor, --[[-----]] { desc = "Debug: [R]un [t]o the Cursor" })
+map.set('n', '<leader>drc', dap.continue, --[[----------]] { desc = "Debug: [R]un [C]ontinue" })
+map.set('n', '<leader>drs', dap.terminate, --[[---------]] { desc = "Debug: [R]un [S]top" })
+map.set('n', '<leader>drp', dap.pause, --[[-------------]] { desc = "Debug: [R]un [P]ause" })
+-- Steps
+map.set('n', '<leader>dso', dap.step_over, --[[---------]] { desc = "Debug: [S]tep [O]ver" })
+map.set('n', '<leader>dsi', step_into, --[[-------------]] { desc = "Debug: [S]tep [I]nto" })
+map.set('n', '<leader>dst', dap.step_out, --[[----------]] { desc = "Debug: [S]tep Ou[t]" })
+map.set('n', '<leader>dsb', dap.step_back, --[[---------]] { desc = "Debug: [S]tep [B]ack" })
+-- Breakpoints
+map.set('n', '<leader>dbt', dap.toggle_breakpoint, --[[-]] { desc = "Debug: Set [B]reakpoint [T]oggle" })
+map.set('n', '<leader>dbc', conditional_breakpoint, --[[]] { desc = "Debug: Set [B]reakpoint with [C]onditions" })
+map.set('n', '<leader>dbl', log_breakpoint, --[[--------]] { desc = "Debug: Set [B]reakpoint with [L]og message" })
+map.set('n', '<leader>dbe', dap.set_exception_breakpoints, { desc = "Debug: Set [B]reakpoint on [E]xception" })
+map.set('n', '<leader>dbl', dap.list_breakpoints, --[[--]] { desc = "Debug: [B]reakpoints [L]ist" })
+map.set('n', '<leader>dbr', dap.clear_breakpoints, --[[-]] { desc = "Debug: [B]reakpoints [R]emove all" })
+-- Frames
+map.set('n', '<leader>dfp', dap.up, --[[----------------]] { desc = "Debug: [F]rame [P]revious" })
+map.set('n', '<leader>dfn', dap.down, --[[--------------]] { desc = "Debug: [F]rame [N]ew" })
+map.set('n', '<leader>duf', dap.focus_frame, --[[-------]] { desc = "Debug: [U]I [F]rames" })
+map.set('n', '<leader>dfr', dap.restart_frame, --[[-----]] { desc = "Debug: [F]rame [R]estart" })
+-- UI
+map.set('n', '<leader>dut', dapui.toggle, --[[-----------]] { desc = "Debug: UI [T]oggle" })
+map.set('n', '<leader>dus', function() dapui.toggle(1) end, { desc = "Debug: UI [S]ide Bar" })
+map.set('n', '<leader>duc', function() dapui.toggle(2) end, { desc = "Debug: UI [C]onsole" })
+map.set('n', '<leader>dur', function() dapui.toggle(3) end, { desc = "Debug: UI [R]epl" })
+map.set('n', '<leader>duf', float_frames, --[[-----------]] { desc = "Debug: UI [F]loating [F]rames" })
+map.set('n', '<leader>dus', float_scopes, --[[-----------]] { desc = "Debug: UI [F]loating [S]copes" })
 
-map.set('n', '<leader>df', function() dap.focus_frame() end, { desc = "Debug: [F]ocus Frame" })
-map.set('n', '<leader>dfr', function() dap.restart_frame() end, { desc = "Debug: [R]estart Frame" })
+map.set({ 'n', 'v' }, '<leader>dh', widgets.hover, --[[--]] { desc = "Debug: [H]over" })
+map.set({ 'n', 'v' }, '<leader>dp', widgets.preview, --[[]] { desc = "Debug: [P]review" })
 
-map.set('n', '<leader>dur', function() dap.repl.toggle() end)
-map.set('n', '<leader>dui', function() dapui.toggle() end)
+dapui.setup({
+    layouts = { {
+        elements = { {
+            id = "scopes",
+            size = 0.25
+        }, {
+            id = "breakpoints",
+            size = 0.25
+        }, {
+            id = "stacks",
+            size = 0.25
+        }, {
+            id = "watches",
+            size = 0.25
+        } },
+        position = "left",
+        size = 40
+    }, {
+        elements = { {
+            id = "console",
+            size = 0.5
+        } },
+        position = "bottom",
+        size = 10
+    }, {
+        elements = {
+            {
+                id = "repl",
+                size = 0.5
+            },
+        },
+        position = "bottom",
+        size = 10
+    }
+    }
+})
 
-vim.keymap.set({ 'n', 'v' }, '<leader>dh', function()
-    widgets.hover()
-end)
-vim.keymap.set({ 'n', 'v' }, '<leader>dp', function()
-    widgets.preview()
-end)
-vim.keymap.set('n', '<leader>df', function()
-    widgets.centered_float(widgets.frames)
-end)
-vim.keymap.set('n', '<leader>ds', function()
-    widgets.centered_float(widgets.scopes)
-end)
-
-dapui.setup()
 dap.listeners.before.attach.dapui_config = function()
     dapui.open()
 end
-dap.listeners.before.launch.dapui_config = function()
-    dapui.open()
+dap.listeners.before.launch.dapui_config = function(config)
+    if config.config.mainClass:match(".*junit.*") then
+        dapui.open(2)
+        dapui.open(3)
+    else
+        dapui.open(2)
+    end
 end

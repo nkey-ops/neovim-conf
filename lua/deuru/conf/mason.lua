2ac2769e97a6322fdcac0a3b31404156c6cad5c3
nkey-ops <terrythebear746@gmail.com>
2025-06-02 20:31:00 +0400

    update: mason-lspconfig handlers replaced with native

--- lua/deuru/conf/mason.lua
+++ lua/deuru/conf/mason.lua
@@ -20,10 +20,44 @@ for _, package_name in ipairs(packages) do
     end
 end
 
-local lspconfig = require("lspconfig")
+vim.lsp.config("lua_ls", {
+    on_init = function(client)
+        if client.workspace_folders then
+            local path = client.workspace_folders[1].name
+            if vim.uv.fs_stat(path .. '/.luarc.json') or vim.uv.fs_stat(path .. '/.luarc.jsonc') then
+                return
+            end
+        end
+
+        client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+            runtime = {
+                -- Tell the language server which version of Lua you're using
+                -- (most likely LuaJIT in the case of Neovim)
+                version = 'LuaJIT'
+            },
+            -- Make the server aware of Neovim runtime files
+            workspace = {
+                checkThirdParty = false,
+                -- library = {
+                --     vim.env.VIMRUNTIME,
+                --     -- Depending on the usage, you might want to add additional paths here.
+                --     "${3rd}/luv/library",
+                --     "${3rd}/busted/library",
+                --     "/home/local/.local/share/nvim/lazy/mason-lspconfig.nvim",
+                --     "/home/local/table/extended-marks.nvim/"
+                -- },
+                -- -- or pull in all of 'runtimepath'. NOTE: this is a lot slower and will cause issues when working on your own configuration (see https://github.com/neovim/nvim-lspconfig/issues/3189)
+                library = vim.api.nvim_get_runtime_file("", true)
+            },
+        })
+    end,
+    settings = {
+        Lua = {}
+    }
+})
+
 require('mason-lspconfig').setup(
     {
-        automatic_installation = true,
         ensure_installed = {
             "marksman",
             "lemminx",
@@ -34,53 +68,10 @@ require('mason-lspconfig').setup(
             "sqlls",
             "html",
         },
-        handlers = {
-            marksman = lspconfig.marksman.setup({}),
-            limminx = lspconfig.lemminx.setup({
-                -- eclipse-lemminx/lemminx.git
-                cmd = { 'java', '-jar', vim.fn.glob('~/.config/dotfiles/nvim/addons/org.eclipse.lemminx-uber.jar') },
-            }),
-            jdtls = nil,
-            -- jdtls = require("lspconfig").jdtls.setup({}),
-            jsonls = lspconfig.jsonls.setup({}),
-            yamlss = lspconfig.yamlls.setup({}),
-            sqlls = lspconfig.sqlls.setup({}),
-            html = lspconfig.html.setup({}),
-            lua_ls =
-                lspconfig.lua_ls.setup {
-                    on_init = function(client)
-                        if client.workspace_folders then
-                            local path = client.workspace_folders[1].name
-                            if vim.uv.fs_stat(path .. '/.luarc.json') or vim.uv.fs_stat(path .. '/.luarc.jsonc') then
-                                return
-                            end
-                        end
-
-                        client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
-                            runtime = {
-                                -- Tell the language server which version of Lua you're using
-                                -- (most likely LuaJIT in the case of Neovim)
-                                version = 'LuaJIT'
-                            },
-                            -- Make the server aware of Neovim runtime files
-                            workspace = {
-                                checkThirdParty = false,
-                                -- library = {
-                                --     vim.env.VIMRUNTIME,
-                                --     -- Depending on the usage, you might want to add additional paths here.
-                                --     "${3rd}/luv/library",
-                                --     "${3rd}/busted/library",
-                                --     "/home/local/.local/share/nvim/lazy/mason-lspconfig.nvim",
-                                --     "/home/local/table/extended-marks.nvim/"
-                                -- },
-                                -- -- or pull in all of 'runtimepath'. NOTE: this is a lot slower and will cause issues when working on your own configuration (see https://github.com/neovim/nvim-lspconfig/issues/3189)
-                                library = vim.api.nvim_get_runtime_file("", true)
-                            },
-                        })
-                    end,
-                    settings = {
-                        Lua = {}
-                    }
-                }
-        }
-    })
+        automatic_enable = {
+            exclude = {
+                "jdtls"
+            }
+        },
+    }
+)

01a84b1415dd19e9e690f1ae926ac9912969b16a
nkey-ops <terrythebear746@gmail.com>
2026-05-09 18:29:16 +0400

    added: luasnips

--- lua/deuru/conf/luasnip.lua
+++ lua/deuru/conf/luasnip.lua
@@ -6,6 +6,8 @@ local i = ls.insert_node
 local rep = extras.rep
 local fmt = require("luasnip.extras.fmt").fmt
 
+local opts = {delimiters = "<>"}
+
 ls.add_snippets("markdown",
     {
         s("env", fmt([[
@@ -17,21 +19,33 @@ ls.add_snippets("markdown",
             i(2),
             rep(1),
         })),
-        s("$", fmt("$\\Large {}$", { i(1) })),
-        s("$$", fmt([[
-            $$\large
-            {}
-            $$
-        ]], { i(1) })),
+        s("$", fmt("${}$", { i(1) })),
+        s("$$", fmt("$${}$$", { i(1) })),
         s(":$", fmt("\\:${}$:\\", { i(1) })),
         s("t", fmt("\\times {}", { i(1) })),
         s("d", fmt("\\cdot {}", { i(1) })),
         s("im", fmt("\\implies {}", { i(1) })),
+        s("cr", fmt([[\circ]], {}, {})),
+        s("par", fmt([[\parallel]], {}, {})),
+
         s("nq", fmt("\\neq {}", { i(1) })),
         s("lq", fmt("\\leq {}", { i(1) })),
         s("gq", fmt("\\geq {}", { i(1) })),
+
+        s("an", fmt([[\angle{<>}]], { i(1) }, opts)),
         s("tr", fmt("\\triangle{{{}}}", { i(1) })),
         s("ol", fmt("\\overline{{{}}}", { i(1) })),
+        s("ln", fmt("\\overleftrightarrow{{{}}}", { i(1) })),
+        s("arc", fmt("\\overset{{\\frown}}{{{}}}", { i(1) })),
+        s("sr", fmt([[\sqrt{<>}]], { i(1) }, { delimiters = "<>" })),
+
+        s("sin", fmt([[\sin(<>)]], { i(1) }, { delimiters = "<>" })),
+        s("cos", fmt([[\cos(<>)]], { i(1) }, { delimiters = "<>" })),
+        s("tan", fmt([[\tan(<>)]], { i(1) }, { delimiters = "<>" })),
+        s("asin", fmt([[\arcsin(<>)]], { i(1) }, { delimiters = "<>" })),
+        s("acos", fmt([[\arccos(<>)]], { i(1) }, { delimiters = "<>" })),
+        s("atan", fmt([[\arctan(<>)]], { i(1) }, { delimiters = "<>" })),
+
         s("fr", fmt(
             [[
             \frac{{{}}}{{{}}}
